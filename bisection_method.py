ef51af72387ba442b7d51fa9280bbb3757973298
NevilleVH <1nevillevh@gmail.com>
2015-03-18 18:45:16 +0200

    added iteration limit

--- bisection_method.py
+++ bisection_method.py
@@ -1,11 +1,11 @@
 import random
-
-num = 0
+import math #so the user can specify functions like math.sin(x) or math.sqrt(x)
 cont = 'Y'
+max_iter = 1e5 #limits the number of tries for finding starting values in case function has no real roots
 while cont == 'Y':
     #user inputs function
     try:
-        f = eval('lambda x : ' + input('f(x) = ').replace('^','**'))
+        f = eval('lambda x : ' + input('f(x) = ').replace('^','**')) 
         f(0)
     except:
         print('Invalid function: Please try again.')
@@ -13,7 +13,8 @@ while cont == 'Y':
     
     #generates initial values (can get stuck if the function has many tpts)
     x_0 = x_1 = 0
-    while f(x_0) > 0 or f(x_1) < 0:   
+    i = 0
+    while (f(x_0) > 0 or f(x_1) < 0) and (i < max_iter):   
         num = random.random() * 100 # using random nums can help escape local minima; not ideal
         if f(x_0) > 0 : #*
             if f(x_0) > f(x_0 - num):
@@ -25,7 +26,12 @@ while cont == 'Y':
                 x_1 -= num
             else:
                 x_1 += num
-  
+        i += 1
+        
+    if i == max_iter: #loop terminated, couldn't cross x-axis
+        print('Either your function has no real roots or they\'re really far away. Try another one!')
+        continue
+    
     #bisect, iterate
     w = (x_0+x_1)/2
     while abs(f(w)) > 1e-10 :
