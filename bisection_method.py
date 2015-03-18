e865b23777b2a1a7c4871f6559d402b7009abb35
NevilleVH <1nevillevh@gmail.com>
2015-03-19 00:05:21 +0200

    upper bounds for real roots

--- bisection_method.py
+++ bisection_method.py
@@ -2,23 +2,28 @@ import random
 import math #so the user can specify functions like math.sin(x) or math.sqrt(x)
 
 def has_roots(s): #descarte's rule of signs: won't work for transcendentals
-    def check_signs(s):
-        func = s
-        if func.find('-') > func.find('x') or func.find('-') == -1: 
-            func = '+' + func
-            pos = func.find('-')
-            if pos > -1: 
-                return True
+    def check_signs(s, sign):
+        count = 0
+        if sign == '+':
+            sign = '-'
         else:
-            pos = func.find('+')
-            if pos > -1: 
-                return True  
+            sign = '+'
+        pos = s.find(sign)
+        if pos > -1:
+            count += 1 + check_signs(s[pos:],sign)
+        return count
+    
     result = [0,0]
+    
     s = s.replace(' ','') 
-    if check_signs(s):
-        result[0] +=1
+    if s.find('-') > s.find('x') or s.find('-') == -1: 
+        s = '+' + s    
+        result[0] = check_signs(s, '+')
+    else:
+        result[0] = check_signs(s,'-')
     
     func = (s.replace('+','&')).replace('-','&') + '&'
+    
     new = ''
     while func != '&':
         pos = func.find('&',1)
@@ -32,8 +37,19 @@ def has_roots(s): #descarte's rule of signs: won't work for transcendentals
         if f1(1) != f2(1):
             substr = substr.replace('x','-x')
         new += substr
-    if check_signs(new):
-        result[1] +=1
+    func = new
+    while  ((new.find('+') > new.find('-') and new.find('+') < new.find('x')) or  (new.find('-') > new.find('+') and new.find('-') < new.find('x'))) and  (func.find('-') != -1 and func.find('+') != -1):
+        if (new.find('-') > new.find('+') and new.find('-') < new.find('x')):
+            new = new[new.find('-'):]
+            func = func[1+new.find('-'):]
+        else: #(new.find('+') > new.find('-') and new.find('+') < new.find('x')) and new.find('-') > -1 and new.find('+') > -1:
+            new =  new[new.find('+'):]
+            func = func[1+new.find('+'):]            
+    if new.find('-') > new.find('x') or new.find('-') == -1: 
+        new = '+' + new    
+        result[1] = check_signs(new, '+')
+    else:
+        result[1] = check_signs(new,'-')
     return result
 
 def get_init_values():
@@ -74,13 +90,10 @@ while cont == 'Y':
         continue
     
     #if the function is a polynomial, determine whether it has real roots <- only correct if tpt on same side as real roots (picks up complex roots)
+    
     roots = has_roots(s)
-    if roots[0] and roots[1]:
-        print('The complex and real root(s) of your function are both positive and negative')
-    elif roots[0]:
-        print('The complex and real root(s) of your function are only positive')
-    elif roots[1]:
-        print('The complex and real root(s) of your function are only negative')
+    if roots[0] or roots[1]:
+        print('Upper bounds for real roots: {} positive and {} negative'.format(roots[0], roots[1]))
     elif s.find('math.') == -1 and f(0) != 0:
             print('Your function has no real roots! Try another one.')
             continue    
